77492b5f44121098a4f0112d66af4451aca707e4
Marc Katz <marc.f.katz@gmail.com>
2023-05-13 18:17:04 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, nameStart){
+    return drivers.filter(function(driver){
+        return driver.slice(0,nameStart.length) === nameStart;
+    });
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+        return driver.name === name;
+    });
+}
